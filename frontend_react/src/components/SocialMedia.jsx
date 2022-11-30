777476f93062cbc1ddf8ec00e5a903780dd08f72
Graham Greer <grahamgreer.pt@gmail.com>
2022-11-30 14:19:53 +0000

    update of social links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,17 +5,17 @@ import { MdEmail } from 'react-icons/md';
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <a href={`https://github.com/Graham-Greer`} target="about_blank">
+      <a href={`https://github.com/graham-greer`} target="about_blank">
         <BsGithub />
       </a>
     </div>
     <div>
-      <a href={`https://www.linkedin.com/in/graham-greer-08006764/`} target="about_blank">
+      <a href={`https://www.linkedin.com/in/graham-greer/`} target="about_blank">
         <BsLinkedin />
       </a>
     </div>
     <div>
-      <a href={`mailto: grahamgreer.pt@gmail.com`}>
+      <a href={`mailto: contact@grahamgreer.co.uk`}>
         <MdEmail />
       </a>
     </div>
